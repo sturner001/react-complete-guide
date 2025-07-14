32b557fb119d3937c02b815c98213cbc0517fc1c
Sam Turner <sam.turner@kokadjo.net>
2025-07-14 16:20:51 -0400

    redux store and class based redux

---
+++ src/store/index.js
@@ -0,0 +1,29 @@
+import { createStore } from 'redux';
+
+const counterReducer = (state = { counter: 0 }, action) => {
+    if (action.type === 'increment') {
+        return {
+            counter: state.counter + 1,
+           // amount: state.amount
+        }
+    }
+    if (action.type === 'decrement') {
+        return {
+            counter: state.counter - 1,
+            //amount: state.amount
+        }
+    }
+  // if (action.type === 'increase') {
+  //     return {
+  //         counter: state.counter + state.amount
+
+  //     }
+  // }
+
+    return state;
+
+};
+
+const store = createStore(counterReducer);
+
+export default store;
