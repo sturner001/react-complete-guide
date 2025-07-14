7b3014b94e7cbe19c39ac8e07b0452440513062f
Sam Turner <sam.turner@kokadjo.net>
2025-07-14 17:41:20 -0400

    hooked up redux to hide counter functionality

--- src/store/index.js
+++ src/store/index.js
@@ -1,24 +1,31 @@
 import { createStore } from 'redux';
 
-const counterReducer = (state = { counter: 0 }, action) => {
+const counterReducer = (state = { counter: 0, show: true }, action) => {
     if (action.type === 'increment') {
         return {
             counter: state.counter + 1,
-           // amount: state.amount
-        }
+            show: state.show
+        };
     }
     if (action.type === 'decrement') {
         return {
             counter: state.counter - 1,
-            //amount: state.amount
-        }
+            show: state.show
+        };
     }
-  // if (action.type === 'increase') {
-  //     return {
-  //         counter: state.counter + state.amount
+    if (action.type === 'increase') {
+        return {
+            counter: state.counter + action.amount,
+            show: state.show
+        };
+    }
 
-  //     }
-  // }
+    if (action.type === 'show') {
+        return {
+            show: !state.show,
+            counter: state.counter
+        };
+    }
 
     return state;
 
